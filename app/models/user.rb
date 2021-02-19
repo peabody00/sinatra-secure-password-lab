61bffb24d9717a9f77231016a86dd8cddc975434
Charlie <peabody00@gmail.com>
2021-02-19 09:04:28 -0600

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ActiveRecord::Base
     has_secure_password
+
+    def balance=(balance = 0)
+        self.balance = balance
+    end
+
+    def deposit(amount)
+        self.balance += amount
+        self.balance
+    end
+
+    def withdrawal(amount)
+        if self.balance >= amount
+            self.balance -= amount
+        else
+            "You are too poor to withdraw money"
+        end
+        self.balance
+    end
+    
 end
